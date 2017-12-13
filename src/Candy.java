5549d262fc14925945a5ccc7e8647574bad00385
slatz8075 <slatz8075@HRH-IC0027332.wrdsb.ca>
2017-12-13 12:15:09 -0500

    still working on things

--- src/Candy.java
+++ src/Candy.java
@@ -20,18 +20,27 @@ public class Candy extends DessertItem {
         this.pricePerLbs = pricePerLbs;
     }
     
-    //create a function that returns the cost of candy based on its ammount
-    public int getCost(double weight, double pricePerLbs){
+    //create a function that returns the cost of candy in cents
+    public int getCost(){
         //multiply the weight (in pounds) with the price per pound
-        return weight*pricePerLbs;
+        return (int)((weight*pricePerLbs)*100);
     }
 
-    //this is the method that prints out the receipt that corresponds to candy
+    //this is the method that prints out the part of the receipt that corresponds to candy
     public String toString() {
         //print out the first line: with its weight in pounds, and its price per pound
         String output = this.weight + " lbs. @ $" + this.pricePerLbs + " /lb" + "\n";
-        //now calculate
-        output += super.getName() + "\n";
+        //now add the name of the candy
+        output += super.getName();
+        //now add spaces to this line but leave room for the total cost at the end of the line
+        //(big blurb in middle is check to see if i is less that the receipt width minus candy name plus the cost of candy)
+        for(int i = 0; i < (DessertShoppe.RECEIPT_WIDTH - (super.getName().length() + DessertShoppe.cents2dollarsAndCents(getCost()).length())); i++){
+            //add one space to between the name and the total
+            output += " ";
+        }
+        //finally add the total of this candy purchase
+        output += getCost();
+        //return the formatted recipt part
         return output;
     }
 }
--- src/TestShoppe.java
+++ src/TestShoppe.java
@@ -13,9 +13,9 @@ public class TestShoppe {
     Checkout checkout = new Checkout();
     
     checkout.enterItem(new Candy("Peanut Butter Fudge", 2.25, 399));
-    checkout.enterItem(new IceCream("Vanilla Ice Cream",105));
-    checkout.enterItem(new Sundae("Choc. Chip Ice Cream",145, "Hot Fudge", 50));
-    checkout.enterItem(new Cookie("Oatmeal Raisin Cookies", 4, 399));
+    //checkout.enterItem(new IceCream("Vanilla Ice Cream",105));
+    //checkout.enterItem(new Sundae("Choc. Chip Ice Cream",145, "Hot Fudge", 50));
+    //checkout.enterItem(new Cookie("Oatmeal Raisin Cookies", 4, 399));
     
     System.out.println(checkout);
     
@@ -24,10 +24,10 @@ public class TestShoppe {
     System.out.println("\n");
     
     
-    checkout.enterItem(new IceCream("Strawberry Ice Cream",145));
-    checkout.enterItem(new Sundae("Vanilla Ice Cream",105, "Caramel", 50));
+    //checkout.enterItem(new IceCream("Strawberry Ice Cream",145));
+    //checkout.enterItem(new Sundae("Vanilla Ice Cream",105, "Caramel", 50));
     checkout.enterItem(new Candy("Gummy Worms", 1.33, 89));
-    checkout.enterItem(new Cookie("Chocolate Chip Cookies", 4, 399));
+    //checkout.enterItem(new Cookie("Chocolate Chip Cookies", 4, 399));
     checkout.enterItem(new Candy("Salt Water Taffy", 1.5, 209));
     checkout.enterItem(new Candy("Candy Corn",3.0, 109));
    
